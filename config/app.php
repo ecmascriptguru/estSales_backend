8e9e123f67da107b30ae31fc720ebb9e39d4e33f
ecmascriptguru <ecmascript.guru@gmail.com>
2017-05-27 15:24:18 -0400

    jwt-auth enabled in system

--- config/app.php
+++ config/app.php
@@ -181,6 +181,11 @@ return [
          *  Voygaer package provider
          */
         TCG\Voyager\VoyagerServiceProvider::class,
+
+        /**
+         *  Tymon/jwt-auth
+         */
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
     ],
 
     /*
@@ -230,6 +235,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
     ],
 
 ];
