bbdaeba2925ba1e5943180bb6d4b60361ca38bd1
ecmascriptguru <ecmascript.guru@gmail.com>
2017-05-27 10:10:00 -0400

    voyager assets installed

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         *  Voygaer package provider
+         */
+        TCG\Voyager\VoyagerServiceProvider::class,
     ],
 
     /*
