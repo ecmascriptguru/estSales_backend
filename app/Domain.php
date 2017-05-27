f1d5746069e2e3281c7cab5cbcf7770ab2f195f5
ecmascriptguru <ecmascript.guru@gmail.com>
2017-05-27 12:21:10 -0400

    something changed

--- app/Domain.php
+++ app/Domain.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Domain extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+    public function initialSamples() {
+        return $this->hasMany('App\InitialSample', 'domain_id');
+    }
 }
--- app/InitialSample.php
+++ app/InitialSample.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class InitialSample extends Model
 {
-    //
+    protected $fillable = ['domain_id', 'bsr', 'est'];
+
+    public function domain() {
+        return $this->belongsTo('App\Domain', 'domain_id');
+    }
 }
