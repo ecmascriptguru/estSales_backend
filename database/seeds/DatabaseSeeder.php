3e1627b88dea30fbbb8b7171c8bb9d3f5b1946da
ecmascriptguru <ecmascript.guru@gmail.com>
2017-05-27 13:22:56 -0400

    db seeder created for voyager and sample data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        // $this->call(DomainsTableSeeder::class);
+        $this->call(VoyagerDatabaseSeeder::class);
+        $this->call(VoyagerDummyDatabaseSeeder::class);
+        $this->call(DomainsTableSeeder::class);
         $this->call(InitialSamplesTableSeeder::class);
     }
 }
