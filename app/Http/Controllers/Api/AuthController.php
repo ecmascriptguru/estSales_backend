583196b132cf3508bd8d8b717ebb4ccdf0b10518
ecmascriptguru <ecmascript.guru@gmail.com>
2017-05-27 22:14:07 -0400

    register / login api created.

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Http\Request;
+use App\User;
+use Response;
+use Hash;
+use JWTAuth;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Create session in api side
+     *
+     * @return json object
+     */
+    public function signup() {
+        $credentials = Input::only('name', 'email', 'password');
+        $credentials['password'] = Hash::make($credentials['password']);
+
+        try {
+            $user = User::create($credentials);
+        } catch (Exception $e) {
+            return Response::json(['status' => false, 'message' => 'User already exists.']);
+        }
+
+        $token = JWTAuth::fromUser($user);
+
+        return Response::json(
+            array('status' => true, 'token' => $token, 'user' => $user)
+        );
+    }
+
+    /**
+     * Create session in api side
+     *
+     * @return json object
+     */
+    public function signin() {
+        $credentials = Input::only('email', 'password');
+
+        if ( ! $token = JWTAuth::attempt($credentials)) {
+            return Response::json(['status' => false, 'message' => 'Credentials are incorrect.']);
+        }
+
+        $user = JWTAuth::toUser($token);
+
+        return Response::json(
+            array('status' => true, 'token' => $token, 'user' => $user)
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', 'Api\AuthController@signup')->name('api_register');
+
+Route::post('/login', 'Api\AuthController@signin')->name('api_login');
