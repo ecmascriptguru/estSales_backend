d9bb440e1e8e4068db66e8458ed1dc3b53f75f07
ecmascriptguru <ecmascript.guru@gmail.com>
2017-05-29 18:31:38 -0400

    history save api created.

--- app/Http/Controllers/Api/V1/ProductsController.php
+++ app/Http/Controllers/Api/V1/ProductsController.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Controller;
 
 use App\ProductCategory as Category;
 use App\Product;
+use App\Domain;
+use App\ProductHistory;
 
 class ProductsController extends Controller
 {
@@ -25,6 +27,33 @@ class ProductsController extends Controller
      *  Add a new product
      */
     public function add(Request $request) {
+        $domain = $request->input('domain');
+        $category = Category::firstOrCreate(['category_name' => 'Book']);
+        if (empty($domain)) {
+            $domain = "amazon.com";
+        }
+        $domain = Domain::firstOrCreate(['name' => $domain]);
 
+        $productParams = $request->only('title', 'asin');
+        $product = Product::firstOrCreate($productParams);
+
+        $history = new ProductHistory;
+        $history->product_id=  $product->id;
+        $history->bsr = $request->input('bsr');
+        $history->currency = $request->input('currency');
+        $history->price = $request->input('price');
+        $history->est = $request->input('est');
+
+        if ($history->save()) {
+            return Response::json([
+                'status' => true,
+                'id' => $history->id
+            ]);
+        } else {
+            return Response::json([
+                'status' => false,
+                'msg' => "Error occured in SQL."
+            ]);
+        }
     }
 }
